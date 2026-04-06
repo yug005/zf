ac5ced69b718a30ab6a48ce4895d78a21e133906
yug005 <yuga6627@gmail.com>
2026-04-07 00:34:00 +0530

    fix notification module dependency cycle

--- src/modules/notifications/notification.module.ts
+++ src/modules/notifications/notification.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller.js';
 import { NotificationService } from './notification.service.js';
 import { PrismaModule } from '../../prisma/prisma.module.js';
-import { ApiKeyModule } from '../api-key/api-key.module.js';
 
 @Module({
-  imports: [PrismaModule, ApiKeyModule],
+  imports: [PrismaModule],
   controllers: [NotificationController],
   providers: [NotificationService],
   exports: [NotificationService],
